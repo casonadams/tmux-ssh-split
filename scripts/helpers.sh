85f9bc18114e9f8ddce8f32c1022c968e92322e0
Cason Adams <casonadams@gmail.com>
2022-01-18 23:47:32 -0700

    options to change default bind keys

---
+++ scripts/helpers.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+split_bind_vertical='"'
+split_bind_vertical_option="@split_bind_vertical"
+
+split_bind_horizontal='%'
+split_bind_horizontal_option="@split_bind_horizontal"
+
+# helper functions
+get_tmux_option() {
+  local option="$1"
+  local default_value="$2"
+  local option_value
+  option_value=$(tmux show-option -gqv "$option")
+  if [ -z "$option_value" ]; then
+    echo "$default_value"
+  else
+    echo "$option_value"
+  fi
+}
+
+split_bind_vertical_key() {
+  get_tmux_option "$split_bind_horizontal_option" "$split_bind_horizontal"
+}
+
+split_bind_horizontal_key() {
+  get_tmux_option "$split_bind_vertical_option" "$split_bind_vertical"
+}
--- tmux-ssh-split.tmux
+++ tmux-ssh-split.tmux
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPTS_DIR="${CURRENT_DIR}/scripts"
+HELPERS_DIR="${CURRENT_DIR}/scripts"
 
-tmux bind-key '%' run-shell "$CURRENT_DIR/scripts/tmux_ssh_split.sh -h"
-tmux bind-key '"' run-shell "$CURRENT_DIR/scripts/tmux_ssh_split.sh -v"
+# shellcheck source=scripts/helpers.sh
+source "${HELPERS_DIR}/helpers.sh"
+
+tmux bind-key "$(split_bind_vertical_key)" run-shell "$SCRIPTS_DIR/tmux_ssh_split.sh -h"
+tmux bind-key "$(split_bind_horizontal_key)" run-shell "$SCRIPTS_DIR/tmux_ssh_split.sh -v"
